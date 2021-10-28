e0f33bf11315dadeceb8c3f2761cab1b3890bac0
Lin Minsu <linminsuwastaken@gmail.com>
2021-10-28 15:35:04 -0400

    Features*

--- plot_wrapper/plots.py
+++ plot_wrapper/plots.py
@@ -1,13 +1,11 @@
+from __future__ import annotations
+
+from typing import Union
+
 import matplotlib
 
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-import numpy
-
-
-# plt.plot([1, 2, 3, 4]) <-- requires "container"
-# plt.ylabel('some numbers') <-- function
-# plt.show() <-- end of function(??)
 
 
 class Plot:
@@ -16,15 +14,25 @@ class Plot:
         self.plot_labelx = labelx
         self.plot_labely = labely
 
-    # private vars
-    array_plotx = []
-    list_ploty = []
+        # private vars
+        self.array_plotx = []
+        self.array_ploty = []
 
     # methods
-    def disp_plot(self, plot_label,
-                  list_plotx, list_ploty):
-        plt.plot
-
-        plt.ylabel(self.plot_labelx)
-        plt.xlabel(self.plot_labely)
+    def disp_plot(self):
+        plt.ylabel(self.plot_labely)
+        plt.xlabel(self.plot_labelx)
+        plt.plot(self.array_plotx, self.array_ploty)
         plt.show()
+
+    def add_x(self, value: Union[int, float]):
+        self.array_plotx.append(value)
+
+    def add_y(self, value: Union[int, float]):
+        self.array_ploty.append(value)
+
+    def set_x(self, xlist: list[int, float]):
+        self.array_plotx = xlist
+
+    def set_y(self, ylist: list[int, float]):
+        self.array_ploty = ylist
